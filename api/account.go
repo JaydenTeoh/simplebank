b7483770450994c0b019b4d55a08f1c0ec2916fc
Jayden Teoh <t3ohjingxiang@gmail.com>
2023-02-21 15:25:29 +0800

    implement authentication middleware

--- api/account.go
+++ api/account.go
@@ -2,15 +2,16 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"net/http"
 
 	db "github.com/JaydenTeoh/simplebank/db/sqlc"
+	"github.com/JaydenTeoh/simplebank/token"
 	"github.com/gin-gonic/gin"
 	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
-	Owner    string `json:"owner" binding:"required"`
 	Currency string `json:"currency" binding:"required,currency"`
 }
 
@@ -21,8 +22,9 @@ func (server *Server) createAccount(ctx *gin.Context) {
 		return
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	arg := db.CreateAccountParams{
-		Owner:    req.Owner,
+		Owner:    authPayload.Username,
 		Currency: req.Currency,
 		Balance:  0,
 	}
@@ -53,16 +55,23 @@ func (server *Server) getAccount(ctx *gin.Context) {
 		return
 	}
 
-	if account, err := server.store.GetAccount(ctx, req.ID); err != nil {
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
 		if err == sql.ErrNoRows {
 			ctx.JSON(http.StatusNotFound, errorResponse(err))
 			return
 		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
-	} else {
-		ctx.JSON(http.StatusOK, account)
 	}
+
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if account.Owner != authPayload.Username {
+		err := errors.New("account doesn't belong to the authenticated user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, account)
 }
 
 type listAccountRequest struct {
@@ -77,7 +86,9 @@ func (server *Server) listAccount(ctx *gin.Context) {
 		return
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	arg := db.ListAccountsParams{
+		Owner:  authPayload.Username,
 		Limit:  req.PageSize,
 		Offset: (req.PageID - 1) * req.PageSize,
 	}
--- api/transfer.go
+++ api/transfer.go
@@ -2,10 +2,12 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"net/http"
 
 	db "github.com/JaydenTeoh/simplebank/db/sqlc"
+	"github.com/JaydenTeoh/simplebank/token"
 	"github.com/gin-gonic/gin"
 )
 
@@ -23,10 +25,20 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 		return
 	}
 
-	if !server.ValidAccount(ctx, req.FromAccountID, req.Currency) {
+	fromAccount, valid := server.ValidAccount(ctx, req.FromAccountID, req.Currency)
+	if !valid {
 		return
 	}
-	if !server.ValidAccount(ctx, req.FromAccountID, req.Currency) {
+
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if fromAccount.Owner != authPayload.Username {
+		err := errors.New("from account doesn't belong to the authenticated user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	_, valid = server.ValidAccount(ctx, req.FromAccountID, req.Currency)
+	if !valid {
 		return
 	}
 
@@ -44,20 +56,22 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 	}
 }
 
-func (server *Server) ValidAccount(ctx *gin.Context, accountID int64, currency string) bool {
-	if account, err := server.store.GetAccount(ctx, accountID); err != nil {
+func (server *Server) ValidAccount(ctx *gin.Context, accountID int64, currency string) (db.Account, bool) {
+	account, err := server.store.GetAccount(ctx, accountID)
+	if err != nil {
 		if err == sql.ErrNoRows {
 			ctx.JSON(http.StatusNotFound, errorResponse(err))
-			return false
+			return account, false
 		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return false
-	} else {
-		if account.Currency != currency {
-			err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", account.ID, account.Currency, currency)
-			ctx.JSON(http.StatusBadRequest, errorResponse(err))
-			return false
-		}
+		return account, false
 	}
-	return true
+
+	if account.Currency != currency {
+		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", account.ID, account.Currency, currency)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return account, false
+	}
+
+	return account, true
 }
